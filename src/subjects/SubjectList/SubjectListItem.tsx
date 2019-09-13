f6c85ef0646783d617aa1babb1d44a6029fe0cd0
John O. Paul <oyomthojohnpaul@gmail.com>
2019-09-13 16:37:51 +0300

    [SubjectListItem] Add icon

--- src/subjects/SubjectList/SubjectListItem.tsx
+++ src/subjects/SubjectList/SubjectListItem.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import styles from "./SubjectList.module.scss";
 import Button from "../../common/buttons/Button";
+import { ReactComponent as BulbIcon } from "../../common/icons/bulb.svg";
 
 const SubjectListItem = ({ children }) => {
   return (
     <li className={styles.item}>
-      <Button className={styles.itemButton}>{children}</Button>
+      <Button className={styles.itemButton}>
+        <div className={styles.icon}>
+          <BulbIcon />
+        </div>
+        <span>{children}</span>
+      </Button>
     </li>
   );
 };
