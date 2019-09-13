2033cb27a4800cb20ec9ae502f64927ae526d2df
John O. Paul <oyomthojohnpaul@gmail.com>
2019-09-13 15:57:18 +0300

    Add styles

--- src/subjects/SubjectList/SubjectListItem.tsx
+++ src/subjects/SubjectList/SubjectListItem.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import styles from "./SubjectList.module.scss";
+import Button from "../../common/buttons/Button";
 
 const SubjectListItem = ({ children }) => {
-  return <li className={styles.item}>{children}</li>;
+  return (
+    <li className={styles.item}>
+      <Button className={styles.itemButton}>{children}</Button>
+    </li>
+  );
 };
 
 export default SubjectListItem;
